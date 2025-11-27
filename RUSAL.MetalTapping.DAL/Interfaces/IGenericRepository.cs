8402cdf44a7b53c087d5adc2901277f9a571253b
Andrey <andreyka.korobkov.97@mail.ru>
2025-11-27 23:43:23 +0700

    Added GenericRepository class

--- RUSAL.MetalTapping.DAL/Interfaces/IGenericRepository.cs
+++ RUSAL.MetalTapping.DAL/Interfaces/IGenericRepository.cs
@@ -2,6 +2,11 @@
 {
     public interface IGenericRepository<TEntity> where TEntity : class
     {
-
+        void Create(TEntity item);
+        TEntity FindById(int id);
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Remove(TEntity item);
+        void Update(TEntity item);
     }
 }
---
+++ RUSAL.MetalTapping.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RUSAL.MetalTapping.DAL.Interfaces;
+
+namespace RUSAL.MetalTapping.DAL.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _context;
+        DbSet<TEntity> _DbSet;
+
+        public GenericRepository(AppDbContext context) {
+            _context = context;
+            _DbSet = context.Set<TEntity>();
+        }
+        public void Create(TEntity item) 
+        {
+            _DbSet.Add(item);
+            _context.SaveChanges();
+        }
+        public TEntity FindById(int id)
+        {
+            return _DbSet.Find(id);
+        }
+        public IEnumerable<TEntity> Get() 
+        {
+            return _DbSet.ToList();
+        }
+        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate) 
+        {
+            return _DbSet.Where(predicate).ToList();
+        }
+        public void Remove(TEntity item)
+        { 
+            _DbSet.Remove(item);
+            _context.SaveChanges();
+        }
+        public void Update(TEntity item) 
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
