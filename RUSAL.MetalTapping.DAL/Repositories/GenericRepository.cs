f5b43e8ccbe3ca364cc6a8e1c2aeab3e2609abb2
striction10 <ilia_shcherbakov1234@mail.ru>
2025-12-04 19:27:42 +0700

    example bll layer

--- RUSAL.MetalTapping.DAL/Repositories/GenericRepository.cs
+++ RUSAL.MetalTapping.DAL/Repositories/GenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RUSAL.MetalTapping.DAL.Interfaces;
+using System.Linq.Expressions;
 
 namespace RUSAL.MetalTapping.DAL.Repositories
 {
@@ -12,32 +13,33 @@ namespace RUSAL.MetalTapping.DAL.Repositories
             _context = context;
             _DbSet = context.Set<TEntity>();
         }
-        public void Create(TEntity item) 
+        public async Task CreateAsync(TEntity item) 
         {
-            _DbSet.Add(item);
-            _context.SaveChanges();
+            await _DbSet.AddAsync(item);
+            await _context.SaveChangesAsync();
         }
-        public TEntity FindById(int id)
+        public async Task<TEntity?> FindByIdAsync(int id)
         {
-            return _DbSet.Find(id);
+            return await _DbSet.FindAsync(id);
         }
-        public IEnumerable<TEntity> Get() 
+        public async Task<IEnumerable<TEntity>> GetAsync()
         {
-            return _DbSet.ToList();
+            return await _DbSet.ToListAsync();
         }
-        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate) 
+        public async Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate) 
         {
-            return _DbSet.Where(predicate).ToList();
+            return await _DbSet.Where(predicate).ToListAsync();
         }
-        public void Remove(TEntity item)
-        { 
+        public async Task RemoveAsync(TEntity item)
+        {
             _DbSet.Remove(item);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
-        public void Update(TEntity item) 
+
+        public async Task UpdateAsync(TEntity item)
         {
             _context.Entry(item).State = EntityState.Modified;
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
     }
 }
